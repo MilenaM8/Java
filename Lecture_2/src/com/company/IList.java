34c94290eae51cabafe44e42e9b9c465c2601bf7
MuzykaMilena <76119202+MilenaM8@users.noreply.github.com>
2021-12-22 00:48:08 +0200

    Delete Lecture_2 directory

--- Lecture_2/src/com/company/IList.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface IList<T> {
-    public void add(T data) throws Exception;
-    public void remove(int index) throws Exception;
-    public boolean contains(T data) throws Exception;
-    public int size();
-    public void showList() throws Exception;
-}
